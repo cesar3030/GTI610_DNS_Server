0c0b35452d00485e1c8715598dd03d5af8a58c36
César Jeanroy <cesar.jeanroy@gmail.com>
2015-07-06 14:34:00 -0400

    We check the bit that indicate if the request is an answer or a response then we choose to do the traitment needed for the request

--- src/com/ets/server/UDPReceiver.java
+++ src/com/ets/server/UDPReceiver.java
@@ -123,18 +123,19 @@ public class UDPReceiver extends Thread {
 	public void run() {
 		try {
 
-            System.out.println(port);
-           port=1589;
             DatagramSocket serveur = new DatagramSocket(this.port); // *Creation d'un socket UDP
+
 
             QueryFinder queryFinder = new QueryFinder(DNSFile);
 		
 			
 			// *Boucle infinie de recpetion
 			while (!this.stop) {
+
 				byte[] buff = new byte[0xFF];
 				DatagramPacket paquetRecu = new DatagramPacket(buff,buff.length);
-				System.out.println("Serveur DNS  "+serveur.getLocalAddress()+"  en attente sur le port: "+ serveur.getLocalPort());
+				//System.out.println("Serveur DNS  "+serveur.getLocalAddress()+"  en attente sur le port: "+ serveur.getLocalPort());
+                System.out.println("Serveur DNS  "+this.SERVER_DNS+"  en attente sur le port: "+ serveur.getLocalPort());
 
 				// *Reception d'un paquet UDP via le socket
 				serveur.receive(paquetRecu);
@@ -147,97 +148,64 @@ public class UDPReceiver extends Thread {
 
 				ByteArrayInputStream TabInputStream = new ByteArrayInputStream (paquetRecu.getData());
 
-				// ****** Dans le cas d'un paquet requete *****
 
-					// *Lecture du Query Domain name, a partir du 13 byte
+                //We read the request id from the 2 first bytes
+                int requestId=0;
+                requestId+=TabInputStream.read();
+                requestId+=TabInputStream.read();
 
+                //We read the request type
+                int requestType;
 
+                //We read the next byte
+                byte byteTmp = (byte) TabInputStream.read();
+                //we convert the byte to a string that contains 8 chars
+                String byteString = String.format("%8s",Integer.toBinaryString(byteTmp & 0xFF)).replace(' ', '0');
 
-
-                    //We read the request id from the 2 first bytes
-                    int requestId=0;
-
-                    requestId+=TabInputStream.read();
-                    requestId+=TabInputStream.read();
-
-
-
-                    //we skipped the 10 next bytes to reach the QUESTION Part
-                    TabInputStream.skip(10);
-                
-                     //The char that match with the value of the current byte
-                    char[]currentChart = null;
-
-                    //We read the next byte, this numbers correspond to the next numbers of bytes that need to be read
-                    int byteValue=TabInputStream.read();
-
-                    //We string we store in the domainName from the request
-                    String domainName = "";
-
-                    boolean continute=true;
-
-
-                    while(continute){
-
-                        /*
-                         *   We check if we need to read chars
-                         */
-                        if(byteValue!=0){
-
-                            /**
-                             * We read the number of chars asked
-                             */
-                            for (int i = 0; i < byteValue; i++) {
-                                currentChart = Character.toChars(TabInputStream.read());
-                                domainName+=currentChart[0];
-                            }
-
-                            //Then we check if we still need to read other chars
-                            byteValue = TabInputStream.read();
-
-
-                            if(byteValue>0){
-                                /*
-                                * If it's yes we add a dot between the first part of
-                                * the domain name and the one who arrived
-                                */
-                                domainName+=".";
-                            }
-                            else{
-                                // we stop
-                                continute=false;
-                            }
-
-
-                        }
-                    }
-
-                System.out.println(domainName);
+                //We check if the first bit of the byte is a 0 or a 1
+                if(byteString.charAt(0) == '0'){
+                    requestType = 0;
+                    System.out.println("\n---- Request ----");
+                }
+                else{
+                    requestType = 1;
+                    System.out.println("\n---- Answer ----");
+                }
 
 
 
+                if(requestType == 0){
+
+                    // ****** Dans le cas d'un paquet requete *****
+
+                    // *Lecture du Query Domain name, a partir du 13 byte
 
 
+                    //we skipped the 9 next bytes to reach the QUESTION Part
+                    TabInputStream.skip(9);
 
+                    String domainName = getDomainName(TabInputStream);
 
-					// *Sauvegarde du Query Domain name
-					
-					// *Sauvegarde de l'adresse, du port et de l'identifiant de la requete
+                    System.out.println("Le nom de domaine: "+domainName);
+
+                    // *Sauvegarde du Query Domain name
+
+                    // *Sauvegarde de l'adresse, du port et de l'identifiant de la requete
                     InetAddress requesterIP = paquetRecu.getAddress();
                     int requesterPort = paquetRecu.getPort();
 
-					// *Si le mode est redirection seulement
-						// *Rediriger le paquet vers le serveur DNS
-					// *Sinon
-						// *Rechercher l'adresse IP associe au Query Domain name
-						// dans le fichier de correspondance de ce serveur					
+                    // *Si le mode est redirection seulement
+                    // *Rediriger le paquet vers le serveur DNS
+                    // *Sinon
+                    // *Rechercher l'adresse IP associe au Query Domain name
+                    // dans le fichier de correspondance de ce serveur
 
-						// *Si la correspondance n'est pas trouvee
-							// *Rediriger le paquet vers le serveur DNS
-						// *Sinon	
-							// *Creer le paquet de reponse a l'aide du UDPAnswerPaquetCreator
-							// *Placer ce paquet dans le socket
-							// *Envoyer le paquet
+                    // *Si la correspondance n'est pas trouvee
+                    // *Rediriger le paquet vers le serveur DNS
+                    // *Sinon
+                    // *Creer le paquet de reponse a l'aide du UDPAnswerPaquetCreator
+                    // *Placer ce paquet dans le socket
+                    // *Envoyer le paquet
 
                     if (this.RedirectionSeulement){
 
@@ -245,36 +213,48 @@ public class UDPReceiver extends Thread {
                     else{
                         List<String> domainIpList = queryFinder.StartResearch(domainName);
 
-                        if(domainIpList == null){
-
+                        if(domainIpList.size()==0){
+                            //we redirect to another DNS server
+                            System.out.println("On redirige vers google dns !!");
                         }
                         else{
-                            UDPAnswerPacketCreator newAnswer = UDPAnswerPacketCreator.getInstance();
-                            newAnswer.CreateAnswerPacket(paquetRecu.getData(),domainIpList);
-
+                            byte[] newAnswerData = UDPAnswerPacketCreator.getInstance().CreateAnswerPacket(paquetRecu.getData(),domainIpList);
+                            DatagramPacket answer = new DatagramPacket(newAnswerData,newAnswerData.length,requesterIP,requesterPort);
+                            serveur.send(answer);
                         }
                     }
+                }
+                else{
 
-				
-				// ****** Dans le cas d'un paquet reponse *****
-						// *Lecture du Query Domain name, a partir du 13 byte
-						
-						// *Passe par dessus Type et Class
-						
-						// *Passe par dessus les premiers champs du ressource record
-						// pour arriver au ressource data qui contient l'adresse IP associe
-						//  au hostname (dans le fond saut de 16 bytes)
-						
-						// *Capture de ou des adresse(s) IP (ANCOUNT est le nombre
-						// de r�ponses retourn�es)			
-					
-						// *Ajouter la ou les correspondance(s) dans le fichier DNS
-						// si elles ne y sont pas deja
-						
-						// *Faire parvenir le paquet reponse au demandeur original,
-						// ayant emis une requete avec cet identifiant				
-						// *Placer ce paquet dans le socket
-						// *Envoyer le paquet
+                // ****** Dans le cas d'un paquet reponse *****
+                    // *Lecture du Query Domain name, a partir du 13 byte
+
+                    // *Passe par dessus Type et Class
+
+                    // *Passe par dessus les premiers champs du ressource record
+                    // pour arriver au ressource data qui contient l'adresse IP associe
+                    //  au hostname (dans le fond saut de 16 bytes)
+
+                    // *Capture de ou des adresse(s) IP (ANCOUNT est le nombre
+                    // de r�ponses retourn�es)
+
+                    // *Ajouter la ou les correspondance(s) dans le fichier DNS
+                    // si elles ne y sont pas deja
+
+                    // *Faire parvenir le paquet reponse au demandeur original,
+                    // ayant emis une requete avec cet identifiant
+                    // *Placer ce paquet dans le socket
+                    // *Envoyer le paquet
+
+                    TabInputStream.skip(12);
+
+                    List<String> IPList = getIpAddressFromANCOUNT(TabInputStream);
+
+                }
+
+                System.out.println("---- END ----\n\n");
+
+
 			}
 //			serveur.close(); //closing server
 		} catch (Exception e) {
@@ -282,4 +262,99 @@ public class UDPReceiver extends Thread {
 			e.printStackTrace(System.err);
 		}
 	}
+
+    /**
+     * Method that return the domain name. It needs to be call when the ByteArrayInputStream is at the index 12.
+     * @param TabInputStream
+     * @return a string that contain the domain name extracted from the request
+     */
+    private String getDomainName(ByteArrayInputStream TabInputStream){
+
+        //We read the next byte, this numbers correspond to the next numbers of bytes that need to be read
+        int byteValue=TabInputStream.read();
+
+        //We store in the domainName from the request
+        String domainName = "";
+
+        //The char that match with the value of the current byte
+        char[]currentChart = null;
+
+        while(true){
+
+            /*
+             *   We check if we need to read chars
+             */
+            if(byteValue!=0){
+
+                /**
+                 * We read the number of chars asked
+                 */
+                for (int i = 0; i < byteValue; i++) {
+                    currentChart = Character.toChars(TabInputStream.read());
+                    domainName+=currentChart[0];
+                }
+
+                //Then we check if we still need to read other chars
+                byteValue = TabInputStream.read();
+
+
+                if(byteValue>0){
+                                /*
+                                * If it's yes we add a dot between the first part of
+                                * the domain name and the one who arrived
+                                */
+                    domainName+=".";
+                }
+                else{
+                    break;
+                }
+
+
+            }
+        }
+
+        return domainName;
+    }
+
+    /**
+     * Method that return the list of IP address that have been resolved for the given domain name.
+     * The method has to be called than the current byte of the ByteArrayInputStream read is 15
+     * @param TabInputStream
+     * @return  The list of IP Address
+     */
+    private List<String> getIpAddressFromANCOUNT(ByteArrayInputStream TabInputStream){
+
+        List<String> ipAddress = new ArrayList<String>();
+
+        //We read the next byte, this numbers correspond to the numbers of IP that need to be extracted
+        int nbIpAddress=TabInputStream.read();
+
+        //we store the current IP address from the request
+        String currentIpAddress = "";
+
+
+        for (int i=1; i < 4*nbIpAddress+1; i++){
+
+            int tmp = TabInputStream.read();
+
+            if(i%4 == 0) {
+               tmp = TabInputStream.read();
+               currentIpAddress+= Integer.toString(tmp);
+               ipAddress.add(currentIpAddress);
+               currentIpAddress="";
+            }
+            else {
+                tmp = TabInputStream.read();
+                currentIpAddress+= Integer.toString(tmp);
+                currentIpAddress+=".";
+            }
+
+        }
+
+        return ipAddress;
+
+    }
+
+
+
 }
